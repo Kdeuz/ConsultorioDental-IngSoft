5450b22802f85083630349496d479280e7cfdf13
Kdeuz <afernandezargoul@gmail.com>
2026-05-10 04:35:21 +0200

    Cambio de oauth2client

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 4000 //el 4000 de TiDB
+    port: process.env.DB_PORT || 4000,
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    }
 });
 
 // 2. CONFIGURACIÓN DE OAUTH2 Y MAILS
